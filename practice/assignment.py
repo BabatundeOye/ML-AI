494ab6ef7827dab8e6162e62e1ad620ba2e31f8d
BabatundeOye <108598015+BabatundeOye@users.noreply.github.com>
2025-02-13 14:43:05 -0700

    still working

---
+++ practice/assignment.py
@@ -0,0 +1,26 @@
+
+#rock paper, scissors
+
+print("Welcome!!!, Let's get ready to play,   \n Rock, Paper, Scissors")
+
+import random
+items = ["rock", "paper", "scissors"]
+player_a = random.choice(items)
+print(player_a)
+
+player_b = input("choose, rock or paper or scissors: ")
+
+
+
+"""def player(b):
+    choice = input("select rock or paper or scissors: ")
+
+b = "bob"
+
+player(b)"""
+
+"""
+if player_a == player_b:
+    print("it is a tie, no winner")"""
+
+
